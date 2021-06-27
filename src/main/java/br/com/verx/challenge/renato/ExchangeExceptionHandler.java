7f9697b62a64228d73cddf17dd2df8cf62592174
Renato <Renato@DESKTOP-TR0G92I>
2021-06-27 09:51:59 -0300

    Generic exception handling

--- src/main/java/br/com/verx/challenge/renato/ExchangeExceptionHandler.java
+++ src/main/java/br/com/verx/challenge/renato/ExchangeExceptionHandler.java
@@ -3,25 +3,35 @@ package br.com.verx.challenge.renato;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.com.verx.challenge.renato.web.rest.BasicResponse;
 import br.com.verx.challenge.renato.web.rest.ErrorResponse;
 
-@ControllerAdvice
+@Component
+@RestControllerAdvice
 public class ExchangeExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	private static Logger log = LoggerFactory.getLogger(ExchangeExceptionHandler.class);
 
 	
 	@ExceptionHandler(ExchangeException.class)
-	protected ResponseEntity<BasicResponse> handleExchangeException(ExchangeException e) {
+	protected ResponseEntity<ErrorResponse> handleExchangeException(ExchangeException e) {
 		log.error(e.getMessage(), e);
 		
 		return ResponseEntity
 				.status(e.getType().getHttpStatus())
 				.body(new ErrorResponse(e.getType().getErrorCode(), e.getMessage()));
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	protected ResponseEntity<ErrorResponse> handleException(RuntimeException e) {
+		log.error(e.getMessage(), e);
+		
+		return ResponseEntity
+				.status(ErrorType.UNEXPECTED_ERROR.getHttpStatus())
+				.body(new ErrorResponse(ErrorType.UNEXPECTED_ERROR.getErrorCode(), "I'm deeply sorry. 3 hired monkeys are already working out the problem"));
 	}
 }
